94a0311be4e7c96f8d7b53a28ba1dc52bad83016
Ryan Grunest <ryangrunest45@gmail.com>
2020-06-04 08:56:22 -0700

    initial commit

---
+++ messingAround/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace messingAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            char character = 'A';
+            var a = 'a';
+            //string firstName = "Ryan";
+            //bool isWorking = true;
+
+            Console.WriteLine(character.CompareTo(a));
+        }
+    }
+}
